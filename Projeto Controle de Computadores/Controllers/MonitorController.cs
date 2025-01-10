d2768d275b4c51b13d71bbcd3e3cfa53004f4659
Guilherme <Guilherme@DESKTOP-38UUC6O>
2025-01-10 16:33:40 -0400

    Adicionar arquivos de projeto.

---
+++ Projeto Controle de Computadores/Controllers/MonitorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MonitorController : ControllerBase {
+
+    private readonly List<string> computadores = new List<string> { "192.168.183.64", "192.168.201.01", "192.168.201.90" };
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<string>>> Get() {
+
+        var resultados = new List<string>();
+        var ping = new Ping();
+
+        foreach (var ip in computadores) {
+            try {
+                var resposta = await ping.SendPingAsync(ip);
+                resultados.Add($"{ip}: {(resposta.Status == IPStatus.Success ? "Online" : "Offline")}");
+            } catch (PingException ex) {
+                resultados.Add($"{ip}: Erro - {ex.Message}");
+            } catch (Exception ex) {
+                resultados.Add($"{ip}: Erro - {ex.Message}");
+            }
+        }
+
+        return resultados;
+    }
+}
---
+++ Projeto Controle de Computadores/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup {
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment evn) {
+        if (evn.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        } else {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+
+        });
+    }
+}
